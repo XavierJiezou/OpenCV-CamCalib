28dd92773620e4bc57d48fb2c55aba64383248a1
XavierJiezou <878972272@qq.com>
2022-08-05 17:58:27 +0800

    update

--- opencv_camcalib/gui/config.py
+++ opencv_camcalib/gui/config.py
@@ -1,2 +1,14 @@
-icon_path = "images/favicon_256x256.ico"
+import os
+import sys
+
+
+def resource_path(relative_path: str) -> None:
+    if getattr(sys, "frozen", False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+icon_path = resource_path("images/favicon_256x256.ico")
 main_size = (800, 600)
